6ea32be1c6b27143b73203a6d3e279671d13bb1f
Jasper Frumau <jasper@imagewize.com>
2025-10-29 10:40:23 +0700

    client reviews block

---
+++ patterns/testimonials/client-reviews-orange.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Pattern: Client Reviews Orange
+ * Category: testimonials
+ *
+ * @package Callandor
+ * @since 1.0.0
+ */
+
+return array(
+	'title'         => __( 'Client Reviews - Orange Background', 'callandor' ),
+	'description'   => __( 'Three client reviews with profile images on vibrant orange background with white text', 'callandor' ),
+	'categories'    => array( 'testimonials' ),
+	'keywords'      => array( 'testimonial', 'review', 'client', 'quote', 'social-proof', 'reviews', 'orange', 'profile' ),
+	'viewportWidth' => 1200,
+	'content'       => '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"},"margin":{"top":"0","bottom":"0"}},"color":{"background":"#ff6b35"}},"textColor":"base","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull has-base-color has-text-color has-background" style="background-color:#ff6b35;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
+	<!-- wp:group {"layout":{"type":"constrained","wideSize":"900px"}} -->
+	<div class="wp-block-group">
+		<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontWeight":"600"},"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}},"textColor":"base","fontSize":"3xl","fontFamily":"open-sans"} -->
+		<h2 class="wp-block-heading has-text-align-center has-base-color has-text-color has-open-sans-font-family has-3-xl-font-size" style="margin-bottom:var(--wp--preset--spacing--50);font-weight:600">' . __( 'Client Reviews.', 'callandor' ) . '</h2>
+		<!-- /wp:heading -->
+
+		<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+		<div class="wp-block-columns alignwide">
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:group {"layout":{"type":"constrained","contentSize":"380px","justifyContent":"center"}} -->
+			<div class="wp-block-group">
+				<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center","orientation":"vertical"}} -->
+				<div class="wp-block-group">
+					<!-- wp:image {"width":"95px","aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"100px"}}} -->
+					<figure class="wp-block-image size-full is-resized has-custom-border"><img src="' . CALLANDOR_PLUGIN_URL . 'assets/images/patterns/dall-e-profile-stylish-male.webp" alt="' . esc_attr__( 'Client profile', 'callandor' ) . '" style="border-radius:100px;aspect-ratio:1;object-fit:cover;width:95px"/></figure>
+					<!-- /wp:image -->
+				</div>
+				<!-- /wp:group -->
+
+				<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}},"textColor":"base","fontSize":"lg","fontFamily":"open-sans"} -->
+				<p class="has-text-align-center has-base-color has-text-color has-open-sans-font-family has-lg-font-size" style="margin-top:var(--wp--preset--spacing--40)">"' . __( 'We have hired Jasper a couple of times and he always does a great job and in a timely manner! He is very good at what he does and we continue to use him for our projects.', 'callandor' ) . '"</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:group {"layout":{"type":"constrained","contentSize":"380px","justifyContent":"center"}} -->
+			<div class="wp-block-group">
+				<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center","orientation":"vertical"}} -->
+				<div class="wp-block-group">
+					<!-- wp:image {"width":"95px","aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"100px"}}} -->
+					<figure class="wp-block-image size-full is-resized has-custom-border"><img src="' . CALLANDOR_PLUGIN_URL . 'assets/images/patterns/dall-e-profile image-male.webp" alt="' . esc_attr__( 'Client profile', 'callandor' ) . '" style="border-radius:100px;aspect-ratio:1;object-fit:cover;width:95px"/></figure>
+					<!-- /wp:image -->
+				</div>
+				<!-- /wp:group -->
+
+				<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}},"textColor":"base","fontSize":"lg","fontFamily":"open-sans"} -->
+				<p class="has-text-align-center has-base-color has-text-color has-open-sans-font-family has-lg-font-size" style="margin-top:var(--wp--preset--spacing--40)">"' . __( 'His communication was top-notch, he met all deadlines, and his skills were very strong. He was proficient in WordPress, WooCommerce, Shopify and programming on those platforms to get our new Shopify site up and running.', 'callandor' ) . '"</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:group {"layout":{"type":"constrained","contentSize":"380px","justifyContent":"center"}} -->
+			<div class="wp-block-group">
+				<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center","orientation":"vertical"}} -->
+				<div class="wp-block-group">
+					<!-- wp:image {"width":"95px","aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"100px"}}} -->
+					<figure class="wp-block-image size-full is-resized has-custom-border"><img src="' . CALLANDOR_PLUGIN_URL . 'assets/images/patterns/dall-e-stylized-illustration-woman-in-a-classic-en-profile-image.webp" alt="' . esc_attr__( 'Client profile', 'callandor' ) . '" style="border-radius:100px;aspect-ratio:1;object-fit:cover;width:95px"/></figure>
+					<!-- /wp:image -->
+				</div>
+				<!-- /wp:group -->
+
+				<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}},"textColor":"base","fontSize":"lg","fontFamily":"open-sans"} -->
+				<p class="has-text-align-center has-base-color has-text-color has-open-sans-font-family has-lg-font-size" style="margin-top:var(--wp--preset--spacing--40)">"' . __( 'Couldn\'t have done this job without Jasper and he did a great job. My website now runs faster than ever. Would definitely hire again.', 'callandor' ) . '"</p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+		</div>
+		<!-- /wp:columns -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->',
+);
